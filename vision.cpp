06a5f6afe2d7af826e190314dbf8a309767ecfcc
James Moran <jamezmoran@googlemail.com>
2016-06-03 01:05:27 +0100

    implemented using atan2

--- vision.cpp
+++ vision.cpp
@@ -8,10 +8,10 @@ const int MAP_HEIGHT = 30;
 
 char map[MAP_WIDTH][MAP_HEIGHT];
 
-const int player_x = 14;
-const int player_y = 29;
+const int player_x = 15;
+const int player_y = 2;
 const int obstr_x = 14;
-const int obstr_y = 25;
+const int obstr_y = 3;
 
 void build_map(){
   map[player_x][player_y] = 'o';
@@ -21,8 +21,11 @@ void build_map(){
 void build_vision(){
   double gradient = 0;
   bool vertical_up = false, vertical_down = false;
-  int rel_x = player_x - obstr_x;
-  int rel_y = player_y - obstr_y;
+  int rel_x = obstr_x - player_x;
+  int rel_y = obstr_y - player_y;
+  if (rel_x == 0 and rel_y == 0)
+    return;
+
   if (rel_x != 0)
     gradient = (double) rel_y / rel_x;
   else if (rel_y > 0)
@@ -38,6 +41,42 @@ void build_vision(){
   double left_edge_gradient = (double)rel_y / (player_x - left_edge);
   double right_edge_gradient = (double)rel_y / (player_x - right_edge);
 
+  double angle = atan2(rel_y, rel_x);
+  double rotation_angle = angle;// + (angle>PI/2 ? PI/2 : 0);
+  double left_edge_x = obstr_x + 0.5*cos(rotation_angle);
+  double right_edge_x = obstr_x - 0.5*cos(rotation_angle);
+  double left_edge_y = obstr_y - 0.5*sin(rotation_angle);
+  double right_edge_y = obstr_y + 0.5*sin(rotation_angle);
+  double left_edge_angle = atan2(left_edge_y - player_y, left_edge_x - player_x);
+  double right_edge_angle = atan2(right_edge_y - player_y, right_edge_x - player_x);
+
+  if (angle > 0 && angle < PI){
+    for (int y = obstr_y+1; y < MAP_HEIGHT; ++y){
+      bool vertical = (obstr_x == player_x) ? true : false;
+      for (int x = vertical ? obstr_x :  obstr_x + std::round((y - obstr_y) / tan(angle)); x >= 0; --x){
+        if (x >= MAP_WIDTH)
+          x = MAP_WIDTH-1;
+        double tile_angle_to_edge = atan2(y - left_edge_y, x - left_edge_x);
+        printf("%G %i %i\n", tile_angle_to_edge, x, y);
+        if (tile_angle_to_edge <= left_edge_angle){
+          map[x][y] = '@';
+        } else {
+          break;
+        }
+      }
+      for (int x = vertical ? obstr_x : obstr_x + std::round((y - obstr_y) / tan(angle)); x < MAP_WIDTH; ++x){
+        if (x < 0)
+          x = 0;
+        double tile_angle_to_edge = atan2(y - right_edge_y, x - right_edge_x);
+        if (tile_angle_to_edge >= right_edge_angle){
+          map[x][y] = '@';
+        } else {
+          break;
+        }
+      }
+    }
+  }
+/*
   if (gradient > 0){
     for (double x = (obstr_x-1/gradient), y = obstr_y-1; y >= 0; --y, x -= 1/gradient){
       int closest_x_index = std::round(x);
@@ -75,11 +114,12 @@ void build_vision(){
     }
   }
   printf("\n");
+  */
 }
 
 int main(){
   for (int i = 0; i < MAP_WIDTH; ++i){
-    for (int j = 0; j < MAP_HEIGHT; ++j){
+    for (int j = MAP_HEIGHT-1; j >= 0; --j){
       map[i][j] = '.';
     }
   }
@@ -88,7 +128,7 @@ int main(){
   build_vision();
 
 
-  for (int j = 0; j < MAP_HEIGHT; ++j){
+  for (int j = MAP_HEIGHT-1; j >= 0; --j){
     for (int i = 0; i < MAP_WIDTH; ++i){
       printf("%c ", map[i][j]);
     }
